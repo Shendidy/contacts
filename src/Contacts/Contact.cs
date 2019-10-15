bb7d02b32e214833aa862ee3509effe19dcf874c
Sherif Shendidy <Sherif.Shendidy@mandg.com>
2019-10-15 10:11:14 +0100

    Started setting up Contact class properties

--- src/Contacts/Contact.cs
+++ src/Contacts/Contact.cs
@@ -2,6 +2,23 @@ namespace Contacts
 {
     public class Contact
     {
-        
+        public Contact(string name)
+        {
+            setName(name);
+        }
+        string name { get; set; }
+        int mobile { get; set; }
+        string dob { get; set; }
+        string address1 { get; set; }
+        string address2 { get; set; }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public string getName()
+        {
+            return this.name;
+        }
     }
 }
